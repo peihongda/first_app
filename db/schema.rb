1a8cbd0c9032aec98329b6a878bfeed33d2945cd
peihongda <pei_hongda@126.com>
2015-08-10 04:20:51 +0000

    Add email unique and test cases

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601070047) do
+ActiveRecord::Schema.define(version: 20150809101857) do
 
   create_table "high_scores", force: :cascade do |t|
     t.string   "game"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(version: 20150601070047) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "user@example.com")}
+  before { @user = User.new(name: "Example User", email: "user1@example.com")}
   subject { @user }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
@@ -44,7 +44,11 @@ describe User do
   
   describe "when email address is already taken" do
     before do
-    
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
     end
+    
+    it { should_not be_valid }
   end
 end
