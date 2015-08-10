25e83abf8c5dc354e8e5889ce0cffb3861242048
peihongda <pei_hongda@126.com>
2015-08-10 08:21:59 +0000

    add password confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "user1@example.com")}
+  before { @user = User.new(name: "Example User", email: "user1@example.com", password: "foobar", password_confirmation: "foobar") }
   subject { @user }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   it { should be_valid }
   
   describe "when name is not present" do
@@ -50,6 +52,21 @@ describe User do
       user_with_same_email.save
     end
     
+    it { should_not be_valid }
+  end
+  
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " "}
+    it { should_not be_valid }
+  end
+  
+  describe "when password doesn't mathc confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+  
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
 end
