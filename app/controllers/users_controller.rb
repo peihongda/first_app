d7c0b46483387b473afdcaa5f73e00c36e7ec82f
peihongda <pei_hongda@126.com>
2015-08-16 09:25:17 +0000

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -14,9 +17,35 @@ class UsersController < ApplicationController
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-    else
+    else  
       render 'new'
     end
   end
   
+  def edit
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user].permit(:name,:email,:password,:password_confirmation))
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+    def signed_in_user
+      unless sign_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in." 
+      end
+    end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
 end
